04c2a798d63b6a83e5ff4459474a86dd978523cc
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2022-10-07 20:42:19 +0300

    upgrade

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,9 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     modules: [
         '@vueuse/nuxt',
         '@nuxtjs/tailwindcss',
-        '@instadapp/vue-web3/nuxt'
+        '@instadapp/vue-web3-nuxt'
     ],
 
     web3 :{
